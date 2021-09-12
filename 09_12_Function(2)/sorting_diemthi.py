e876826d25d5f245029aa8b0e9610e5baa35c9f1
mai <mai.nguyen3@onemount.com>
2021-09-12 14:11:37 +0700

    update xu_ly_chuoi and sorting diem thi

---
+++ 09_12_Function(2)/sorting_diemthi.py
@@ -0,0 +1,28 @@
+# Điểm thi học kỳ của sinh viên được lưu ở định dạng 1 tuple có 3 phần tử (m1, m2, e) gồm:
+# m1 = midterm1
+# m2 = midterm2
+# e = endterm
+# Cho một list gồm danh sách điểm thi của sinh viên 1 lớp. 
+# Viết chương trình Python để sắp xếp danh sách trước theo thứ tự tăng dần theo phần tử cuối cùng trong mỗi tuple (sắp xếp theo điểm cuối kỳ - endterm tăng dần).
+# vd sort_list_last([(1, 2, 5), (9, 1, 2), (6, 4, 4), (3, 2, 3), (10, 2, 1)]) == [(10, 2, 1), (9, 1, 2), (3, 2, 3), (6, 4, 4), (1, 2, 5)]
+
+
+
+#Cach 1: dung lambda trong key cua sorted-------------------------------------------------------
+
+my_list_tup = [(1, 2, 5), (9, 1, 2), (6, 4, 4), (3, 2, 3), (10, 2, 1)]
+
+output = sorted(my_list_tup, key=lambda x: x[-1])
+print ('Sorting list tuple by last element of tup:', output)
+
+#Cach 2: dinh nghia 1 function roi dung function nhu mot doi tuong. truyen vao key cua sorted.---
+
+my_list_tup = [(1, 2, 5), (9, 1, 2), (6, 4, 4), (3, 2, 3), (10, 2, 1)]
+
+def sort_tup_by_last_element(my_list_tup):
+    x = my_list_tup [-1]
+
+    return x
+
+output = sorted(my_list_tup, key = sort_tup_by_last_element)  
+print ('Sorting list tuple by last element of tup:', output)
---
+++ 09_12_Function(2)/xu_ly_chuoi.py
@@ -0,0 +1,91 @@
+# Cho 1 chuỗi A (vd: "tHE fOX iS cOMING fOR tHE cHICKEN"). 
+# Viết hàm đảo ngược thứ tự các từ trong chuỗi và đổi tất cả các chữ cái từ hoa thành thường và ngược lại. (kết quả là "Chicken The For Coming Is Fox The")
+
+ 
+# *********************************************************** Sao *** Mai *************************************************************************************
+# Có 04 cách:
+# Cách 01: Using lamba function instead define function. Bản chất giống hệt cách 02
+# Cách 02: Bản chất muốn loại vòng for để giảm độ phức tạp thuật toán 
+# Cách 03: Dùng 01 vòng for. 
+# Cách 04: Dùng 02 vòng for -> phức tạp nhất.
+
+
+# Cách 1: Using lambda function-----------------------
+
+import re
+
+my_string = "tHE fOX iS cOMING fOR tHE cHICKEN"
+# replace all lower case characters with upper case
+new_string = re.sub(r"[a-zA-Z]",
+                    lambda x :  x.group(0).upper()
+                                if x.group(0).islower()
+                                else x.group(0).lower(),
+                    my_string)
+
+# reverse string after change lower or upper                  
+final_str = ' '.join(reversed(new_string.split(" ")))
+print(final_str)
+
+
+# Cách 2: sử dụng thư viện re. để có thể chuyển all thành hoa thường và ngược lại. Cách này sẽ giúp loại bỏ vòng for -> giảm độ phức tạp.------------------------- 
+import re
+
+def reverse_case(match_obj):
+    char_elem = match_obj.group(0)
+    if char_elem.islower():
+        return char_elem.upper()
+    else:
+        return char_elem.lower()
+my_string = "tHE fOX iS cOMING fOR tHE cHICKEN"
+# replace all lower case characters with upper case 
+new_string = re.sub(r"[a-zA-Z]",reverse_case, my_string)
+# reverse string after change lower or upper
+final_str = ' '.join(reversed(new_string.split(" ")))
+print(final_str)
+
+
+# Cách 2: -----------------------------------------------------------
+my_string = "tHE fOX iS cOMING fOR tHE cHICKEN"
+
+def string_function(my_string):
+    # replace character in tring to lower or uper
+    my_string1 = ""
+    for ch in my_string:
+        if ch.isupper():
+            new_ch = ch.lower()
+        else: 
+            new_ch = ch.upper()
+        my_string1 += new_ch
+
+    # reverse string after change lower or upper
+    new_string = ' '.join(reversed(my_string1.split(" ")))
+
+    return new_string
+
+print(string_function(my_string))
+
+
+# Cách 3: ------------------------------------------------------------
+my_string = "tHE fOX iS cOMING fOR tHE cHICKEN"
+
+def string_function(my_string):
+    # replace character in tring to lower or uper
+    my_string1 = ""
+    for ch in my_string:
+        if ch.isupper():
+            new_ch = ch.lower()
+        else: 
+            new_ch = ch.upper()
+        my_string1 += new_ch
+
+    #Convert string after change lower or upper
+    new_string = my_string1 [::-1].split(" ")
+
+    #Convert each str in tring
+    s = ''
+    for str in new_string:
+        new_str = str [::-1]
+        s += new_str + " "
+
+    return s
+print(string_function(my_string))
